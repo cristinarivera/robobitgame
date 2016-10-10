c1d273105c780db9986aa3e2cb479dbc72eddbbb
nando <nandovm95@gmail.com>
2016-10-10 15:56:27 +0200

    enemy.png

---
+++ game/src/enemy.h
@@ -0,0 +1,10 @@
+// Data created with Img2CPC - (c) Retroworks - 2007-2015
+#ifndef _ASSETS_ENEMY_H_
+#define _ASSETS_ENEMY_H_
+
+#include <types.h>
+#define G_ENEMY_W 8
+#define G_ENEMY_H 27
+extern const u8 g_enemy[8 * 27];
+
+#endif
